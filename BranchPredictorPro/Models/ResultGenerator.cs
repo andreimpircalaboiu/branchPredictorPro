5951ae404d2c6c9dddab59f64a7bda90231dfa68
DESKTOP-MNOGPLK\Radu2T <radu.tode@gmail.com>
2017-01-07 18:05:17 +0200

    result generator

---
+++ BranchPredictorPro/Models/ResultGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchPredictorPro.Models
+{
+    class ResultGenerator
+    {
+        public string TraceName { get; set;}
+        public string TypeName { get; set; }
+        public int Hrl { get; set; }
+        public int Hrg { get; set; }
+        public int Path { get; set; }
+        public double UnbiasedPolarization { get; set; }
+        public string UnbiasedContext { get; }
+
+  
+
+        public string print() {
+
+            return "Trace:                 " + TraceName + "\n" +
+                   "Type:                  " + TypeName + "\n" +
+                   "Hrl:                   " + Hrl + "\n" +
+                   "Hrg:                   " + Hrg + "\n" +
+                   "Path:                  " + Path + "\n" +
+                   "Unbiased Polarization: " + UnbiasedPolarization + "\n" +
+                   "Unbiased Context:      " + UnbiasedContext + "\n";
+
+
+
+
+
+
+        }
+
+
+    }
+}
